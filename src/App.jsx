2d03a1e9933ab00cc7c2b86b80a4941e53df215d
jluchett <jluchett@hotmail.com>
2023-06-04 16:27:21 -0500

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 
 import Navbar from './components/Navbar'
 import './App.css'
+import Footer from './components/Footer'
 
 function App() {
  
-
   return (
     <>
       <Navbar/>
+      <Footer/>
     </>
+    
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,47 @@
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <div className="left-section">
+          <div className="social-media">
+            <h5>Síguenos en nuestras redes sociales:</h5>
+            <ul>
+              <li>
+                <a href="#">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fab fa-instagram"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="right-section">
+          <div className="contact-info">
+            <h4>Contáctanos:</h4>
+            <p>
+              <i className="fab fa-whatsapp"></i> +3012723018
+            </p>
+          </div>
+        </div>
+      </footer>
+      <footer className="footer2">
+        <div className="rights-reserved">
+          <p>Todos los derechos reservados &copy; 2023</p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
