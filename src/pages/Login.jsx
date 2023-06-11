dd444b3f43267f27029226d152580529500338e8
jluchett <jluchett@hotmail.com>
2023-06-11 11:20:17 -0500

    logica del registro

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,39 +1,102 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/logo2.png";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../styles/Login.css"; // Archivo CSS para los estilos del componente
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isValid, setIsValid] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setErrorMessage("");
+    setIsValid("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (email.trim() === "") {
+      setErrorMessage("Debes ingresar un correo electrónico");
+    } else if (!isValidEmail(email)) {
+      setErrorMessage("Digite un correo electrónico valido");
+    } else {
+      setLoading(true);
+      // Aquí puedes realizar la acción de envío del formulario
+      fetch(`http://localhost:3001/users/login?email=${email}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setLoading(false);
+          if (data.success) {
+            // Redirigir a la página de inicio de sesión exitosa
+            
+            navigate("/singin");
+          } else {
+            console.log(data)
+            setIsValid(data.message);
+          }
+        })
+        .catch((error) => {
+          setLoading(false);
+          setIsValid("Error al obtener los datos del usuario");
+          console.log(error);
+        });
+      console.log("Formulario enviado");
+      setIsValid("");
+    }
+  };
+
+  const isValidEmail = (email) => {
+    // Expresión regular para validar el formato de correo electrónico
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
   return (
     <>
       <div className="logo-container">
         <img src={Logo} alt="Logo Rattanmerce" />
         <h1>Rattanmerce</h1>
       </div>
+      {isValid && (
+        <div className="backend-message">
+          <span>Hubo un problema</span>
+          <p>{isValid}</p>
+          <p>Verifica tu correo o registrate para continuar</p>
+        </div>
+      )}
       <div className="login-container">
         <div className="form-container">
           <h2>Inicia Sesión</h2>
-          <form>
-            <input type="email" placeholder="Correo electrónico" />
-            <button type="submit">Enviar</button>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Correo electrónico"
+              value={email}
+              onChange={handleEmailChange}
+            />
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
+            <button type="submit">{loading ? "Cargando..." : "Enviar"}</button>
           </form>
           <Link to="/terminos" className="login-condition">
             <p>Condiciones de uso y aviso de privacidad</p>
           </Link>
         </div>
         <div className="register-container">
-        <p>¿Aún no tienes cuenta?</p>
+          <p>¿Aún no tienes cuenta?</p>
           <Link to="/register">
-          
-          <button>Regístrate aquí</button>
+            <button>Regístrate aquí</button>
           </Link>
-          
         </div>
       </div>
       <div className="footer-container">
         <hr />
-        <div className="footer-links">  
+        <div className="footer-links">
           <a href="#">Términos y condiciones</a>
           <a href="#">Aviso de privacidad</a>
         </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,73 @@
-
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useState } from "react";
 import Logo from "../assets/logo2.png";
 import { Link } from "react-router-dom";
-import '../styles/Register.css'
+import "../styles/Register.css";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const validateEmail = (email) => {
+    // Función de validación de correo electrónico
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validaciones de los campos de entrada
+    if (!validateEmail(email)) {
+      setErrorMessage("Ingresa un correo electrónico válido.");
+      return;
+    }
+
+    if (password.length < 8) {
+      setErrorMessage("La contraseña debe tener al menos 8 caracteres.");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setErrorMessage("Las contraseñas no coinciden.");
+      return;
+    }
+
+    // Realizar el fetch a la base de datos aquí
+    // Puedes usar la función fetch o una librería como axios
+    fetch("http://localhost:3001/users/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Registro exitoso, puedes redirigir a la página de inicio de sesión
+          console.log("respuesta ok")
+        } else {
+          // Manejo de errores, por ejemplo:
+          throw new Error("Error en la solicitud");
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+        setErrorMessage("Ocurrió un error durante el registro.");
+      });
+    // Resetear los campos de entrada y mostrar mensaje de éxito
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+    setErrorMessage("¡Registro exitoso!");
+
+    // Aquí puedes redirigir a la página de inicio de sesión u otra página
+  };
   return (
     <>
       <div className="logo-container">
@@ -15,13 +77,33 @@ const Register = () => {
       <div className="registro-container">
         <div className="form-container">
           <h2>Crea tu cuenta</h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <span>Correo electrónico</span>
-            <input type="email" placeholder="Correo electrónico" />
+            <input
+              type="email"
+              placeholder="Correo electrónico"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <span>Contraseña</span>
-            <input type="password" name="contra" placeholder='Contraseña' />
+            <input
+              type="password"
+              name="contra"
+              placeholder="Debe tener minimo 8 caracteres"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <span>Confirmar contraseña</span>
-            <input type="password" name="confContra" placeholder='Vuelve a escribir la contraseña' />
+            <input
+              type="password"
+              name="confContra"
+              placeholder="Vuelve a escribir la contraseña"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            {errorMessage && (
+              <span className="error-message">{errorMessage}</span>
+            )}
             <button type="submit">Enviar</button>
           </form>
           <Link to="/terminos" className="login-condition">
@@ -29,17 +111,15 @@ const Register = () => {
           </Link>
         </div>
         <div className="register-container">
-        <p>¿Ya tienes cuenta?</p>
+          <p>¿Ya tienes cuenta?</p>
           <Link to="/login">
-          
-          <button>Inicia sesión aquí</button>
+            <button>Inicia sesión aquí</button>
           </Link>
-          
         </div>
       </div>
       <div className="footer-container">
         <hr />
-        <div className="footer-links">  
+        <div className="footer-links">
           <a href="#">Términos y condiciones</a>
           <a href="#">Aviso de privacidad</a>
         </div>
@@ -49,7 +129,7 @@ const Register = () => {
         </p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
